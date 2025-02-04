59c524b33bc45f01564704f60704c10df984ce59
SindreSau <sindre_50@hotmail.com>
2025-02-04 15:58:45 +0100

    feat: Add Prettier and ESLint configuration files with custom rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,40 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:prettier/recommended'),
+    {
+        ignores: ['node_modules', '.next'],
+    },
+    {
+        rules: {
+            'prettier/prettier': [
+                'error',
+                {
+                    singleQuote: true,
+                    jsxSingleQuote: true,
+                    trailingComma: 'es5',
+                    printWidth: 100,
+                    tabWidth: 2,
+                    useTabs: false,
+                    semi: true,
+                    bracketSpacing: true,
+                    arrowParens: 'always',
+                    endOfLine: 'lf',
+                    bracketSameLine: false,
+                    quoteProps: 'as-needed',
+                },
+            ],
+        },
+    },
 ];
 
 export default eslintConfig;
