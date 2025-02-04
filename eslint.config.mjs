23b7cafb7b68a4c26e89adc3bed81bf4e55ccc94
SindreSau <sindre_50@hotmail.com>
2025-02-04 16:20:09 +0100

    style: format of config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,35 +6,35 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-    ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:prettier/recommended'),
-    {
-        ignores: ['node_modules', '.next'],
-    },
-    {
-        rules: {
-            'prettier/prettier': [
-                'error',
-                {
-                    singleQuote: true,
-                    jsxSingleQuote: true,
-                    trailingComma: 'es5',
-                    printWidth: 100,
-                    tabWidth: 2,
-                    useTabs: false,
-                    semi: true,
-                    bracketSpacing: true,
-                    arrowParens: 'always',
-                    endOfLine: 'lf',
-                    bracketSameLine: false,
-                    quoteProps: 'as-needed',
-                },
-            ],
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:prettier/recommended'),
+  {
+    ignores: ['node_modules', '.next'],
+  },
+  {
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          jsxSingleQuote: true,
+          trailingComma: 'es5',
+          printWidth: 100,
+          tabWidth: 2,
+          useTabs: false,
+          semi: true,
+          bracketSpacing: true,
+          arrowParens: 'always',
+          endOfLine: 'lf',
+          bracketSameLine: false,
+          quoteProps: 'as-needed',
         },
+      ],
     },
+  },
 ];
 
 export default eslintConfig;
