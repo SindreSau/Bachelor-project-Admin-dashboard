e26b3af40c2cc3edfd6fabc0be371c1a880a65c7
alexmccorkle <114952775+alexmccorkle@users.noreply.github.com>
2025-02-06 13:51:46 +0100

    feat: updated ApplicationView with application cards and improved layout with mock data for now

--- src/lib/sample-group-data.ts
+++ src/lib/sample-group-data.ts
@@ -4,7 +4,7 @@ export const sampleApplications: Application[] = [
   {
     groupId: 'G001',
     coverLetter:
-      'We are a group at the University of Technology and I am interested in your program.',
+      ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque at purus sit amet pharetra. Suspendisse sit amet orci at massa cursus dignissim ac a diam. Donec sit amet sem hendrerit, condimentum sapien a, pharetra orci. Morbi vitae nibh sit amet lectus aliquet accumsan. Vestibulum quis augue est. Aliquam semper dolor quis bibendum ultricies. Curabitur vulputate urna est, sit amet suscipit nunc dictum quis. Pellentesque egestas elit at ipsum suscipit, in ullamcorper lectus blandit. Mauris a tincidunt neque. Morbi pellentesque lobortis quam, vel feugiat magna elementum non. Phasellus risus diam, tincidunt at placerat non, iaculis a ligula. Pellentesque vestibulum nisi ut risus laoreet blandit. Vivamus feugiat eget est a pellentesque.Sed lobortis rutrum leo eu aliquet. Nulla dapibus risus eget molestie consequat. Aenean ac rutrum quam. Suspendisse lacinia pellentesque lorem, sodales pulvinar arcu vestibulum a. Nulla quis consequat nunc. Cras hendrerit orci id tortor tempus tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla et placerat tellus, ultricies elementum felis. Sed vulputate, odio ac euismod dignissim, massa turpis aliquam nibh, in maximus augue nisi nec lectus. Proin rutrum leo eu orci bibendum, eu tempor nunc consequat. Nulla ut tellus at augue bibendum placerat vitae luctus ipsum. Morbi a felis sit amet augue finibus aliquam ac vitae est. Morbi posuere tortor at mi volutpat sodales. Maecenas et placerat ligula. Proin id urna elit. Phasellus pulvinar rhoncus nisl, et mollis massa interdum id. Sed tristique sit amet libero id varius. Mauris pulvinar nisi a commodo bibendum. Aliquam arcu dui, pretium at rhoncus a, porta id lorem. Donec pellentesque augue orci. Aliquam ut sollicitudin odio. Praesent a iaculis urna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque eget urna et nisl egestas tincidunt vulputate in ligula. Ut rutrum sed lacus sollicitudin. ',
     school: 'University of Technology',
     students: [
       {
@@ -18,7 +18,6 @@ export const sampleApplications: Application[] = [
         firstName: 'Jane',
         lastName: 'Smith',
         email: 'jane.smith@uot.edu',
-        cv: new File([''], 'cv-jane-smith.pdf'),
         grades: new File([''], 'grades-jane-smith.pdf'),
       },
       {
