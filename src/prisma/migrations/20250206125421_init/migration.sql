1d7127a28c41461af99064d2befe219f304fce8e
SindreSau <sindre_50@hotmail.com>
2025-02-06 14:02:40 +0100

    feat: updated database schema -> created a seed.ts that creates and application with one unpublished task and three students that has applied

--- src/prisma/migrations/20250206125421_init/migration.sql
+++ src/prisma/migrations/20250206125421_init/migration.sql
@@ -1,10 +1,15 @@
+-- CreateEnum
+CREATE TYPE "DocumentType" AS ENUM ('CV', 'GRADES');
+
+-- CreateEnum
+CREATE TYPE "ReviewStatus" AS ENUM ('0', '1', '2');
+
 -- CreateTable
 CREATE TABLE "Student" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
-    "applicationId" INTEGER,
     "fileIds" INTEGER[],
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -15,9 +20,8 @@ CREATE TABLE "Student" (
 -- CreateTable
 CREATE TABLE "File" (
     "id" SERIAL NOT NULL,
-    "applicationId" INTEGER NOT NULL,
     "studentId" INTEGER NOT NULL,
-    "documentType" TEXT,
+    "documentType" "DocumentType" NOT NULL,
     "storageUrl" TEXT NOT NULL,
     "fileName" TEXT NOT NULL,
     "uploadedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -32,6 +36,7 @@ CREATE TABLE "Application" (
     "school" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "studentRepresentativeId" INTEGER,
 
     CONSTRAINT "Application_pkey" PRIMARY KEY ("id")
 );
@@ -41,10 +46,24 @@ CREATE TABLE "Task" (
     "id" SERIAL NOT NULL,
     "taskName" TEXT NOT NULL,
     "taskDescription" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "minStudents" INTEGER NOT NULL DEFAULT 3,
+    "maxStudents" INTEGER NOT NULL DEFAULT 5,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Review" (
+    "id" SERIAL NOT NULL,
+    "applicationId" INTEGER NOT NULL,
+    "review" "ReviewStatus",
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -76,7 +95,10 @@ CREATE INDEX "_ApplicationToTask_B_index" ON "_ApplicationToTask"("B");
 ALTER TABLE "File" ADD CONSTRAINT "File_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "Student"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "File" ADD CONSTRAINT "File_applicationId_fkey" FOREIGN KEY ("applicationId") REFERENCES "Application"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Application" ADD CONSTRAINT "Application_studentRepresentativeId_fkey" FOREIGN KEY ("studentRepresentativeId") REFERENCES "Student"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_applicationId_fkey" FOREIGN KEY ("applicationId") REFERENCES "Application"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_StudentToApplication" ADD CONSTRAINT "_StudentToApplication_A_fkey" FOREIGN KEY ("A") REFERENCES "Application"("id") ON DELETE CASCADE ON UPDATE CASCADE;
