5ede45680c6c4ac8a4434ce1ada266adbab18995
SindreSau <sindre_50@hotmail.com>
2025-02-18 12:11:13 +0100

    feat: update date picker to use Norwegian locale and change placeholder text

--- src/components/tasks/date-picker.tsx
+++ src/components/tasks/date-picker.tsx
@@ -27,7 +27,7 @@ export function DatePicker({
           )}
         >
           <CalendarIcon className='mr-2 h-4 w-4' />
-          {value ? format(value, 'PPP') : <span>Pick a date</span>}
+          {value ? format(value, 'PPP') : <span>Velg dato</span>}
         </Button>
       </PopoverTrigger>
       <PopoverContent className='w-auto p-0'>
