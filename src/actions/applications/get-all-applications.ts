897162a1c00696e5893d614b9ed79e25b962c543
alexmccorkle <114952775+alexmccorkle@users.noreply.github.com>
2025-02-24 15:15:45 +0100

    feat: replace getAllApplicationsForTableView with getAllApplications and add getOneApplication function

--- src/actions/applications/get-all-applications.ts
+++ src/actions/applications/get-all-applications.ts
@@ -1,7 +1,7 @@
 'use server';
 import { db } from '@/lib/prisma';
 
-export default async function getAllApplicationsForTableView() {
+export default async function getAllApplications() {
   try {
     const applications = await db.application.findMany({
       include: {
