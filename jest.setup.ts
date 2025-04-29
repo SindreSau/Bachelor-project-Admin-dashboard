c847023760a92398a93667c11761482dbb603838
alexmccorkle <114952775+alexmccorkle@users.noreply.github.com>
2025-04-29 10:46:06 +0200

    test: add unit tests for deleteAllApplications action

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,5 @@
 import '@testing-library/jest-dom';
+
+// Silence console.error and console.warn during tests
+jest.spyOn(console, 'error').mockImplementation(() => {});
+jest.spyOn(console, 'warn').mockImplementation(() => {});
